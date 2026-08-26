eb9114b8783350a0501f77693cb75009ebae15b4
John Sampson <jsamps100@uafs.edu>
2026-08-26 12:09:17 -0500

    Update

--- assignOne/assignOne/Problem1.java
+++ assignOne/assignOne/Problem1.java
@@ -3,14 +3,37 @@ package assignOne;
 public class Problem1 {
 
 	public static void main(String[] args) {
+		String str = "-121";
 		
-
+		System.out.print(stringToInt(str));
+		
 	}
+
 	
-	public static int stringToInt(String str) {
-		
-		
-		return 0;
+		public static int stringToInt(String str) {
+			
+		if (str.charAt(0) == '-'){
+			
+			int totalIntVal = 0;
+			int currentInt = 0;
+			 
+			for(int i=1;i<str.length();i++){
+				currentInt = str.charAt(i) - '0';
+				totalIntVal=(totalIntVal*10) + currentInt;
+			}
+			totalIntVal = (totalIntVal) -(totalIntVal*2);
+			return totalIntVal;
+	
+		}else{
+			
+			int totalIntVal = 0;
+			int currentInt = 0;
+				 
+			for(int i=0;i<str.length();i++){
+				currentInt = str.charAt(i) - '0';
+				totalIntVal=(totalIntVal*10) + currentInt;
+			}
+			return totalIntVal;
+		}
 	}
-
 }
--- assignOne/assignOne/Problem2.java
+++ assignOne/assignOne/Problem2.java
@@ -3,11 +3,20 @@ package assignOne;
 public class Problem2 {
 
 	public static void main(String[] args) {
+		/*int integer = 121;
+		int extractedDigit;
 		
-
+		while (integer > 0)
+			extractedDigit = integer % 10;
+		*/
+		
+		char ch = 3 + '0';
+		System.out.print(ch);
+		
 	}
 	
 	public static String intToString(int value) {
+		
 		
 		
 		return null;
